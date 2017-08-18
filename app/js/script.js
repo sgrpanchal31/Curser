4511cc150a560c171f86cbe9f583a6d6d978cb9e
sgrpanchal31 <sgrpanchal31@gmail.com>
2017-08-18 23:36:32 +0530

    added style to the frontend and some social media buttons

--- app/js/script.js
+++ app/js/script.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
     console.log("hi");
 
     $.ajax({
-        url:"http://localhost:5000/curse/api/v1.0/words/2",
+        url:"http://localhost:5000/curse/api/v1.0/words/",
         type:"GET",
         // data:"actionfunction=showData",
         cache: false,
+
+        asynch:false,
+        contentType: "application/json; charset=utf-8",
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        crossDomain: true,
+        dataType: 'jsonp',
         success: function(response){
-            console.log(response);
+            console.log(response.words);
         }
 
     });
